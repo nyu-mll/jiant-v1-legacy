0af26b0571101a70358951201de189b664a6ebf8
Berlin Chen <berlinchen7@gmail.com>
2018-07-11 20:07:27 +0000

    tweaked experiment scripts to accomodate latest pulls

--- major_experiment_scripts/main_experiment_mt_wo_attn.sh
+++ major_experiment_scripts/main_experiment_mt_wo_attn.sh
@@ -1,9 +1,9 @@
 # Use a run like this as a sanity check to make sure you can run with our current defaults on GPU.
 # Change only the training_task name and exp_name.
-python ../src/main.py --config ../config/test_run.conf --overrides "exp_name = test-run-mt, run_name = test, train_tasks = wmt14_en_de"
+python ../main.py --config ../config/test_run.conf --overrides "exp_name = test-run-mt, run_name = test, train_tasks = wmt14_en_de"
 
 # Then, on Monday, run eight runs like these, changing only the training_task name and exp_name.
-python ../src/main.py --config ../config/defaults.conf --overrides "exp_name = main-mt-wo-attn, train_tasks = wmt14_en_de, run_name = noelmo-do2-sd1, elmo_chars_only = 1, dropout = 0.2"
-python ../src/main.py --config ../config/defaults.conf --overrides "exp_name = main-mt-wo-attn, train_tasks = wmt14_en_de, run_name = noelmo-do4-sd1, elmo_chars_only = 1, dropout = 0.4"
-python ../src/main.py --config ../config/defaults.conf --overrides "exp_name = main-mt-wo-attn, train_tasks = wmt14_en_de, run_name = elmo-do2-sd1, elmo_chars_only = 0, dropout = 0.2"
-python ../src/main.py --config ../config/defaults.conf --overrides "exp_name = main-mt-wo-attn, train_tasks = wmt14_en_de, run_name = elmo-do4-sd1, elmo_chars_only = 0, dropout = 0.4"
+python ../main.py --config ../config/defaults.conf --overrides "exp_name = main-mt-wo-attn, train_tasks = wmt14_en_de, run_name = noelmo-do2-sd1, elmo_chars_only = 1, dropout = 0.2"
+python ../main.py --config ../config/defaults.conf --overrides "exp_name = main-mt-wo-attn, train_tasks = wmt14_en_de, run_name = noelmo-do4-sd1, elmo_chars_only = 1, dropout = 0.4"
+python ../main.py --config ../config/defaults.conf --overrides "exp_name = main-mt-wo-attn, train_tasks = wmt14_en_de, run_name = elmo-do2-sd1, elmo_chars_only = 0, dropout = 0.2"
+python ../main.py --config ../config/defaults.conf --overrides "exp_name = main-mt-wo-attn, train_tasks = wmt14_en_de, run_name = elmo-do4-sd1, elmo_chars_only = 0, dropout = 0.4"
--- major_experiment_scripts/main_experiment_qnli_wo_attn.sh
+++ major_experiment_scripts/main_experiment_qnli_wo_attn.sh
@@ -1,9 +1,9 @@
 # Use a run like this as a sanity check to make sure you can run with our current defaults on GPU.
 # Change only the training_task name and exp_name.
-python ../src/main.py --config ../config/test_run.conf --overrides "exp_name = test-run-qnli-alt, run_name = test, qnli-alt_pair_attn = 0, train_tasks = qnli-alt"
+python ../main.py --config ../config/test_run.conf --overrides "exp_name = test-run-qnli-alt, run_name = test, qnli-alt_pair_attn = 0, train_tasks = qnli-alt"
 
 # Then, on Monday, run eight runs like these, changing only the training_task name and exp_name.
-python ../src/main.py --config ../config/defaults.conf --overrides "exp_name = main-qnli-wo-attn, train_tasks = qnli-alt, qnli-alt_pair_attn = 0, run_name = noelmo-do2-sd1, elmo_chars_only = 1, dropout = 0.2"
-python ../src/main.py --config ../config/defaults.conf --overrides "exp_name = main-qnli-wo-attn, train_tasks = qnli-alt, qnli-alt_pair_attn = 0, run_name = noelmo-do4-sd1, elmo_chars_only = 1, dropout = 0.4"
-python ../src/main.py --config ../config/defaults.conf --overrides "exp_name = main-qnli-wo-attn, train_tasks = qnli-alt, qnli-alt_pair_attn = 0, run_name = elmo-do2-sd1, elmo_chars_only = 0, dropout = 0.2"
-python ../src/main.py --config ../config/defaults.conf --overrides "exp_name = main-qnli-wo-attn, train_tasks = qnli-alt, qnli-alt_pair_attn = 0, run_name = elmo-do4-sd1, elmo_chars_only = 0, dropout = 0.4"
+python ../main.py --config ../config/defaults.conf --overrides "exp_name = main-qnli-wo-attn, train_tasks = qnli-alt, qnli-alt_pair_attn = 0, run_name = noelmo-do2-sd1, elmo_chars_only = 1, dropout = 0.2"
+python ../main.py --config ../config/defaults.conf --overrides "exp_name = main-qnli-wo-attn, train_tasks = qnli-alt, qnli-alt_pair_attn = 0, run_name = noelmo-do4-sd1, elmo_chars_only = 1, dropout = 0.4"
+python ../main.py --config ../config/defaults.conf --overrides "exp_name = main-qnli-wo-attn, train_tasks = qnli-alt, qnli-alt_pair_attn = 0, run_name = elmo-do2-sd1, elmo_chars_only = 0, dropout = 0.2"
+python ../main.py --config ../config/defaults.conf --overrides "exp_name = main-qnli-wo-attn, train_tasks = qnli-alt, qnli-alt_pair_attn = 0, run_name = elmo-do4-sd1, elmo_chars_only = 0, dropout = 0.4"
