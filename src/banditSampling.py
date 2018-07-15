24b3014688901c3208910eb5f39e65c57e26fd9b
Shuning Jin <jinxx596@d.umn.edu>
2018-07-15 19:37:30 -0400

    more bandit

--- src/banditSampling.py
+++ src/banditSampling.py
@@ -1,16 +1,19 @@
 import random
 import numpy as np
+import sys
 
 from allennlp.common import Params
 import copy
 #import matplotlib.pyplot as plt
 
+
+# gradient: self.baseline (0, 'average')
 class Bandit():
     def __init__(self, actions,
                  stepSize = 0.3, initialQ=0.0,
-                 explore_method=None, #'epsilon','gradient'
+                 explore_method=None, #'epsilon','boltzmann', 'gradient'
                  temp=1.0,
-                 epsilon=0.2):
+                 epsilon=0.2,baseline = 0):
 
         self.mapping = dict(enumerate(actions))
         self.k = len(actions)
@@ -25,26 +28,31 @@ class Bandit():
         self.explore_method = explore_method
         self.temp = None; self.epsilon = None
         # boltzmann exploration: softmax temp
-        if explore_method == 'gradient':
+        if explore_method == 'boltzmann':
             self.temp = temp
         # epsilon greedy exploration: epsilon
-        if explore_method == 'epsilon':
+        elif explore_method == 'epsilon':
             self.epsilon = epsilon
+        elif explore_method == 'gradient':
+            self.temp = temp
+            self.rewardAvg = 0
+            self.time = 0
+            self.baseline = baseline
 
         # keep trace
         self.action = None
         self.reward = None
 
     def chooseAction(self):
-        if self.explore_method == 'gradient':
-            self.chooseAction_gradient()
-        if self.explore_method == 'epsilon':
+        if self.explore_method in ['boltzmann', 'gradient']:
+            self.chooseAction_boltzmann()
+        elif self.explore_method == 'epsilon':
             self.chooseAction_epsilonGreedy()
+        else: sys.exit(1)
 
-    def chooseAction_gradient(self):
-        # softmax(nparray,temp)
-        nparray_exp = np.exp(self.Q/self.temp)
-        action_prob = nparray_exp/sum(nparray_exp)
+
+    def chooseAction_boltzmann(self):
+        action_prob = softmax(self.temp,self.Q)
         self.action = random.choices(self.indices,action_prob,k=1)[0]
 
     def chooseAction_epsilonGreedy(self):
@@ -58,7 +66,20 @@ class Bandit():
     def update_actionValue(self, reward):
         self.reward = reward
         index = self.action
-        self.Q[index] += self.stepSize * (reward - self.Q[index])
+
+        if self.explore_method in ['boltzmann','epsilon'] :
+            self.Q[index] += self.stepSize * (reward - self.Q[index])
+
+        elif self.explore_method == 'gradient':
+            baseline = self.baseline
+            if baseline == 'average':
+                self.time +=1
+                self.rewardAvg = (self.rewardAvg*(self.time-1)+reward)/self.time
+                baseline = self.rewardAvg
+            self.Q[index] += stepSize * (reward- baseline) *(1-self.prob[index])
+            for i in self.indices:
+                if i == index: pass
+                self.Q[i] -= stepSize * (reward- baseline) *(self.prob[i])
 
     @classmethod
     def from_params(cls,actions,params):
@@ -98,3 +119,9 @@ def build_bandit_params(args):
         params[attr] = getattr(args, attr)
 
     return Params(params)
+
+
+def softmax(temp, nparray):
+    nparray = nparray - np.max(nparray)
+    nparray_exp = np.exp(nparray/temp)
+    return (nparray_exp/sum(nparray_exp))
