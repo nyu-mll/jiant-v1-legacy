dd8040edf2c1e65bf632faba24f4ee625179c6fa
Berlin Chen <berlinchen7@gmail.com>
2018-07-18 23:09:16 +0000

    Edited to conform to the latest MT interface.

--- major_experiment_scripts/main_experiment_mt_wo_attn.sh
+++ major_experiment_scripts/main_experiment_mt_wo_attn.sh
@@ -3,7 +3,7 @@
 #python ../main.py --config ../config/test_run.conf --overrides "exp_name = test-run-mt, run_name = test, train_tasks = wmt14_en_de"
 
 # Then, on Monday, run eight runs like these, changing only the training_task name and exp_name.
-python ../main.py --config ../config/defaults.conf --overrides "exp_name = main-mt-wo-attn, train_tasks = wmt14_en_de, do_train = 0, train_for_eval = 0, eval_tasks = mnli, run_name = noelmo-do2-sd1, elmo_chars_only = 1, dropout = 0.2"
-python ../main.py --config ../config/defaults.conf --overrides "exp_name = main-mt-wo-attn, train_tasks = wmt14_en_de, do_train = 0, train_for_eval = 0, eval_tasks = mnli, run_name = noelmo-do4-sd1, elmo_chars_only = 1, dropout = 0.4"
-python ../main.py --config ../config/defaults.conf --overrides "exp_name = main-mt-wo-attn, train_tasks = wmt14_en_de, do_train = 0, train_for_eval = 0, eval_tasks = mnli, run_name = elmo-do2-sd1, elmo_chars_only = 0, dropout = 0.2"
-python ../main.py --config ../config/defaults.conf --overrides "exp_name = main-mt-wo-attn, train_tasks = wmt14_en_de, do_train = 0, train_for_eval = 0, eval_tasks = mnli, run_name = elmo-do4-sd1, elmo_chars_only = 0, dropout = 0.4"
+python ../main.py --config ../config/defaults.conf --overrides "exp_name = main-mt-wo-attn, train_tasks = wmt14_en_de, mt_attention=none, do_train = 0, train_for_eval = 0, eval_tasks = mnli, run_name = noelmo-do2-sd1, elmo_chars_only = 1, dropout = 0.2"
+python ../main.py --config ../config/defaults.conf --overrides "exp_name = main-mt-wo-attn, train_tasks = wmt14_en_de, mt_attention=none, do_train = 0, train_for_eval = 0, eval_tasks = mnli, run_name = noelmo-do4-sd1, elmo_chars_only = 1, dropout = 0.4"
+python ../main.py --config ../config/defaults.conf --overrides "exp_name = main-mt-wo-attn, train_tasks = wmt14_en_de, mt_attention=none, do_train = 0, train_for_eval = 0, eval_tasks = mnli, run_name = elmo-do2-sd1, elmo_chars_only = 0, dropout = 0.2"
+python ../main.py --config ../config/defaults.conf --overrides "exp_name = main-mt-wo-attn, train_tasks = wmt14_en_de, mt_attention=none, do_train = 0, train_for_eval = 0, eval_tasks = mnli, run_name = elmo-do4-sd1, elmo_chars_only = 0, dropout = 0.4"
