a31a1f909399a819de06ae2a584c3b29696b850c
Ian Tenney <iftenney@gmail.com>
2018-09-07 04:29:36 +0000

    Clean up SPR2 code, remove absolute paths.

--- probing/data/convert-spr2.py
+++ probing/data/convert-spr2.py
@@ -1,33 +1,53 @@
-import pandas as pd
-import conllu
+#!/usr/bin/env python
+
+import sys
+import os
 import json
+import argparse
 
-def get_ud_corpus_data():
-  '''
-  extracts the underlying UD corpus data that is stored in conllu format.
-  Returns a dictionary where the keys are the split and the values are dictionaries
-  where the keys are the sentenceId
-  '''
-  extracted_data_by_split = {}
-  sentId2text = {}
-  for split in ['train', 'dev', 'test']:
-    extracted_data_by_split[split] = {}
-    data = open("UD_English-EWT-r1.2/en-ud-%s.conllu" % (split)).readlines()
-    data = "".join(data)
-    data = conllu.parse(data)
-    sent_count = 0
-    for sent in data:
-      extracted_data_by_split[split][sent_count] = sent
-      sentId2text[(split, sent_count)] = " ".join([item['form'] for item in sent] )
-      sent_count += 1
+import logging as log
+log.basicConfig(format='%(asctime)s: %(message)s',
+                datefmt='%m/%d %I:%M:%S %p', level=log.INFO)
 
-  return extracted_data_by_split, sentId2text
+import pandas as pd
 
-def convert_spr(ud_corpus):    
+import conllu
 
+def load_ud_corpus(ud_source_dir: str):
+    '''
+    Extracts the underlying UD corpus data that is stored in conllu format.
+    Returns a dictionary where the keys are the split and the values are dictionaries
+    where the keys are the sentenceId
+    '''
+    data_path = os.path.join(ud_source_dir, "UD_English-EWT-r1.2")
+
+    #  extracted_data_by_split = {}
+    sent_id_to_text = {}
+    for split in ['train', 'dev', 'test']:
+        #  extracted_data_by_split[split] = {}
+        split_path = os.path.join(data_path, f"en-ud-{split}.conllu")
+        log.info("Loading UD data from %s", split_path)
+        with open(split_path) as fd:
+            data = "".join(line for line in fd)
+        #  data = open("UD_English-EWT-r1.2/en-ud-%s.conllu" % (split)).readlines()
+        #  data = "".join(data)
+        data = conllu.parse(data)
+        sent_count = 0
+        for sent in data:
+            #  extracted_data_by_split[split][sent_count] = sent
+            sent_id_to_text[(split, sent_count)] = " ".join(
+                [item['form'] for item in sent])
+            sent_count += 1
+
+    return sent_id_to_text
+  #  return extracted_data_by_split, sent_id_to_text
+
+def convert_spr(sent_id_to_text, protoroles_source_dir: str,
+                output_dir: str):
     sent_id2pred_arg_pairs = {}
     sent_id2targets = {}
-    df = pd.read_csv("protoroles_eng_ud1.2_11082016.tsv", sep='\t', header=0)
+    source_file = os.path.join(protoroles_source_dir, "protoroles_eng_ud1.2_11082016.tsv")
+    df = pd.read_csv(source_file, sep='\t', header=0)
     for df_idx, row in df.iterrows():
         if row['Applicable'] == 'no':
             continue
@@ -35,9 +55,9 @@ def convert_spr(ud_corpus):
         assert len(id_pair) == 2
         split = id_pair[0].split(".con")[0].split("-")[-1]
         sent_id = id_pair[1]
-        sent_text = ud_corpus[1][split,int(sent_id)-1]
+        sent_text = sent_id_to_text[split, int(sent_id)-1]
         #Dataset    Is.Pilot    Passes.Filters  Protocol    Split   Annotator.ID    Sentence.ID Pred.Token  Pred.Lemma  Gram.Func   Arg.Phrase  Arg.Tokens.Begin    Arg.Tokens.End  Property    Response    Applicable  Sent.Grammatical
-        #assert " ".join(sent_text.split()[row['Arg.Tokens.Begin']: row['Arg.Tokens.End']+ 1]).lower() == row['Arg.Phrase'].lower(), " ".join(sent_text.split()[row['Arg.Tokens.Begin']: row['Arg.Tokens.End']+ 1]) + "\t||\t" + row['Arg.Phrase'] 
+        #assert " ".join(sent_text.split()[row['Arg.Tokens.Begin']: row['Arg.Tokens.End']+ 1]).lower() == row['Arg.Phrase'].lower(), " ".join(sent_text.split()[row['Arg.Tokens.Begin']: row['Arg.Tokens.End']+ 1]) + "\t||\t" + row['Arg.Phrase']
         if (split, sent_id) not in sent_id2targets:
             sent_id2targets[(split, sent_id)] = {"targets": [],
                                                  "info": {
@@ -73,7 +93,7 @@ def convert_spr(ud_corpus):
     outfiles = {}
     for key in sent_id2targets:
         val = sent_id2targets[key]
-        val["text"] = ud_corpus[1][key[0],int(key[1])-1]
+        val["text"] = sent_id_to_text[key[0], int(key[1])-1]
         val["info"]["split"] = key[0]
         val["info"]["sent_id"] = key[1]
         for span_pair in sent_id2pred_arg_pairs[key]:
@@ -85,13 +105,31 @@ def convert_spr(ud_corpus):
             #for obj in json_objs:
         data_split = val['info']['split']
         if data_split not in outfiles:
-            outfiles[data_split] = open("spr2_edge_probing_%s.json" % (data_split), 'w')
+            output_file = os.path.join(output_dir, f"edges.{data_split}.json")
+            outfiles[data_split] = open(output_file, 'w')
         json.dump(val, outfiles[data_split])
         outfiles[data_split].write('\n')
 
-def main():
-    ud_corpus = get_ud_corpus_data()
-    convert_spr(ud_corpus)
+
+def main(args):
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--src_dir', type=str, required=True,
+                        help="Path to source data (SPR and UD1.2), as passed "
+                             "to get_spr_data.sh")
+    parser.add_argument('-o', dest='output_dir', type=str, required=True,
+                        help="Output directory, e.g. /path/to/edges/data/spr2")
+    args = parser.parse_args(args)
+
+    if not os.path.isdir(args.output_dir):
+        os.mkdir(args.output_dir)
+
+    sent_id_to_text = load_ud_corpus(os.path.join(args.src_dir, "ud"))
+    log.info("Processing proto-role annotations...")
+    convert_spr(sent_id_to_text, os.path.join(args.src_dir, "protoroles"),
+                output_dir=args.output_dir)
+    log.info("Done!")
 
 if __name__ == '__main__':
-    main()
+    main(sys.argv[1:])
+    sys.exit(0)
+
--- probing/data/get_spr2_data.sh
+++ probing/data/get_spr2_data.sh
@@ -1,9 +1,23 @@
-wget http://decomp.net/wp-content/uploads/2015/08/protoroles_eng_pb.tar.gz
-tar zxvf protorole_eng_pb.tar.gz
+#!/bin/bash
 
-wget http://decomp.net/wp-content/uploads/2015/08/protoroles_eng_ud1.2.tar.gz
-tar zxvf protoroles_eng_ud1.2.tar.gz
+TARGET_DIR=$1
 
+pushd $TARGET_DIR
+
+set -e
+
+# wget http://decomp.net/wp-content/uploads/2015/08/protoroles_eng_pb.tar.gz
+# tar zxvf protorole_eng_pb.tar.gz
+
+# wget http://decomp.net/wp-content/uploads/2015/08/protoroles_eng_ud1.2.tar.gz
+# tar zxvf protoroles_eng_ud1.2.tar.gz
+
+# Univeral Dependencies 1.2 for English Web Treebank (ewt) source text.
 wget https://github.com/UniversalDependencies/UD_English/archive/r1.2.tar.gz
-tar -zxvf r1.2.tar.gz
+mkdir ud
+tar -zxvf r1.2.tar.gz -C ud
 
+# Semantic Proto Roles annotations.
+wget http://decomp.io/projects/semantic-proto-roles/protoroles_eng_udewt.tar.gz
+mkdir protoroles
+tar -xvzf protoroles_eng_udewt.tar.gz -C protoroles
