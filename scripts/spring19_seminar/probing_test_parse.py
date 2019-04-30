5d890c14c8546926e18a4ff3acfd7da74b8281e4
Sheng-Fu Wang <shengfu.wang@nyu.edu>
2019-04-29 20:30:53 -0400

    Update probing_test_parse.py

--- scripts/spring19_seminar/probing_test_parse.py
+++ scripts/spring19_seminar/probing_test_parse.py
@@ -9,5 +9,10 @@ for file in os.listdir(result_path):
     exps = [x for x in os.listdir(os.path.join(result_path, file)) if x not in ['vocab', 'results.tsv', 'tasks', 'preproc', 'embs.pkl']]
     print(exps)
     for exp in exps:
-        results = [x for x in os.listdir(os.path.join(result_path, file, exp) if '.tsv' in x]
-        print(results)
+        results = [x for x in os.listdir(os.path.join(result_path, file, exp)) if '.tsv' in x]
+        for result in results:
+            infile = open(os.path.join(result_path, file, exp, result), 'r').readlines()
+            preds = [x.split("\t")[1] for x in infile]
+            labels = [x.split("\t")[4][0] for x in infile]
+            print(matthews_corrcoef(preds, labels))
+            
