582e8ac88cd3ec3060d34d94b9e2c3d5540836c7
Sheng-Fu Wang <shengfu.wang@nyu.edu>
2019-04-29 20:26:25 -0400

    Update probing_test_parse.py

--- scripts/spring19_seminar/probing_test_parse.py
+++ scripts/spring19_seminar/probing_test_parse.py
@@ -6,5 +6,8 @@ import os
 result_path = "/scratch/sfw268/seminar/env1"
 
 for file in os.listdir(result_path):
-    exps = [x for x in os.listdir(os.path.join(result_path, file)) if x not in ['vocab', 'results.tsv', 'tasks', 'preproc']]
+    exps = [x for x in os.listdir(os.path.join(result_path, file)) if x not in ['vocab', 'results.tsv', 'tasks', 'preproc', 'embs.pkl']]
     print(exps)
+    for exp in exps:
+        results = [x for x in os.listdir(os.path.join(result_path, file, exp) if '.tsv' in x]
+        print(results)
