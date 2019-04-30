ddb6e51bdf68ac7f3065c570b8f5b2638a19f54a
Sheng-Fu Wang <shengfu.wang@nyu.edu>
2019-04-29 20:33:42 -0400

    Update probing_test_parse.py

--- scripts/spring19_seminar/probing_test_parse.py
+++ scripts/spring19_seminar/probing_test_parse.py
@@ -5,14 +5,15 @@ import os
 
 result_path = "/scratch/sfw268/seminar/env1"
 
-for file in os.listdir(result_path):
-    exps = [x for x in os.listdir(os.path.join(result_path, file)) if x not in ['vocab', 'results.tsv', 'tasks', 'preproc', 'embs.pkl']]
-    print(exps)
+outfile = open(os.path.join(result_path, 'results.tsv'))
+
+for folder in os.listdir(result_path):
+    exps = [x for x in os.listdir(os.path.join(result_path, folder)) if x not in ['vocab', 'results.tsv', 'tasks', 'preproc', 'embs.pkl']]
     for exp in exps:
-        results = [x for x in os.listdir(os.path.join(result_path, file, exp)) if '.tsv' in x]
+        results = [x for x in os.listdir(os.path.join(result_path, folder, exp)) if '.tsv' in x]
         for result in results:
-            infile = open(os.path.join(result_path, file, exp, result), 'r').readlines()
+            infile = open(os.path.join(result_path, folder, exp, result), 'r').readlines()
             preds = [x.split("\t")[1] for x in infile]
             labels = [x.split("\t")[4][0] for x in infile]
-            print(matthews_corrcoef(preds, labels))
-            
+            outfile.write(folder+'\t'+exp+'\t'+result+'\t'+matthews_corrcoef(preds, labels)+'\n')
+
