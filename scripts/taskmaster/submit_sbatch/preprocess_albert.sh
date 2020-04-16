3024a5be57e0efd3346eb21c6d402d68214ae99d
Jason Phang <email@jasonphang.com>
2020-04-16 19:36:03 -0400

    scripts

---
+++ scripts/taskmaster/submit_sbatch/preprocess_albert.sh
@@ -0,0 +1,11 @@
+PROG="probing/retokenize_edge_data" ARGS="-t albert-xxlarge-v2 ${JIANT_DATA_DIR}/edges/spr1/*.json" sbatch scripts/taskmaster/gcp/cpu.sbatch
+PROG="probing/retokenize_edge_data" ARGS="-t albert-xxlarge-v2 ${JIANT_DATA_DIR}/edges/spr2/*.json" sbatch scripts/taskmaster/gcp/cpu.sbatch
+PROG="probing/retokenize_edge_data" ARGS="-t albert-xxlarge-v2 ${JIANT_DATA_DIR}/edges/dpr/*.json" sbatch scripts/taskmaster/gcp/cpu.sbatch
+PROG="probing/retokenize_edge_data" ARGS="-t albert-xxlarge-v2 ${JIANT_DATA_DIR}/edges/dep_ewt/*.json" sbatch scripts/taskmaster/gcp/cpu.sbatch
+PROG="probing/retokenize_edge_data" ARGS="-t albert-xxlarge-v2 ${JIANT_DATA_DIR}/edges/ontonotes/const/pos/*.json" sbatch scripts/taskmaster/gcp/cpu.sbatch
+PROG="probing/retokenize_edge_data" ARGS="-t albert-xxlarge-v2 ${JIANT_DATA_DIR}/edges/ontonotes/const/nonterminal/*.json" sbatch scripts/taskmaster/gcp/cpu.sbatch
+PROG="probing/retokenize_edge_data" ARGS="-t albert-xxlarge-v2 ${JIANT_DATA_DIR}/edges/ontonotes/srl/*.json" sbatch scripts/taskmaster/gcp/cpu.sbatch
+PROG="probing/retokenize_edge_data" ARGS="-t albert-xxlarge-v2 ${JIANT_DATA_DIR}/edges/ontonotes/ner/*.json" sbatch scripts/taskmaster/gcp/cpu.sbatch
+PROG="probing/retokenize_edge_data" ARGS="-t albert-xxlarge-v2 ${JIANT_DATA_DIR}/edges/ontonotes/coref/*.json" sbatch scripts/taskmaster/gcp/cpu.sbatch
+PROG="probing/retokenize_edge_data" ARGS="-t albert-xxlarge-v2 ${JIANT_DATA_DIR}/edges/semeval/*.json" sbatch scripts/taskmaster/gcp/cpu.sbatch
+PROG="scripts/ccg/align_tags_to_bert" ARGS="-t albert-xxlarge-v2 -d ${JIANT_DATA_DIR}/ccg" sbatch scripts/taskmaster/gcp/cpu.sbatch
